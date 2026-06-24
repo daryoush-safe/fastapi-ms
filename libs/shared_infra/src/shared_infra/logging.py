a1c26ac571b5cc30d2542b9994b566d32d49bc4b
Alos <daryoosh.safe@gmail.com>
2026-06-24 03:59:15 +0330

    refactor: fix ruff lint warn inside logging

--- libs/shared_infra/src/shared_infra/logging.py
+++ libs/shared_infra/src/shared_infra/logging.py
@@ -37,11 +37,7 @@ def configure_logging(
         cache_logger_on_first_use=True,
     )
 
-    renderer = (
-        structlog.processors.JSONRenderer()
-        if json_logs
-        else structlog.dev.ConsoleRenderer()
-    )
+    renderer = structlog.processors.JSONRenderer() if json_logs else structlog.dev.ConsoleRenderer()
 
     formatter = structlog.stdlib.ProcessorFormatter(
         foreign_pre_chain=shared_processors,
