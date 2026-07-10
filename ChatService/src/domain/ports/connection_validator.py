f5dad02bf7c9dc18a7bc3b2add74f173a269fa13
Alos <daryoosh.safe@gmail.com>
2026-07-10 15:29:30 +0330

    chore: handle lint error

--- ChatService/src/domain/ports/connection_validator.py
+++ ChatService/src/domain/ports/connection_validator.py
@@ -4,5 +4,4 @@ from abc import ABC, abstractmethod
 
 class IConnectionValidator(ABC):
     @abstractmethod
-    async def validate(self, connection_id: uuid.UUID, access_token: str) -> None:
-        ...
+    async def validate(self, connection_id: uuid.UUID, access_token: str) -> None: ...
